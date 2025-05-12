94f2366099c47c9d6ca1d6c1e2284614486a4d34
Pau Cornudella Quer <161316282+PauCoqu@users.noreply.github.com>
2025-05-12 18:41:03 +0200

    Create rotar_perfil.m

---
+++ rotar_perfil.m
@@ -0,0 +1,25 @@
+function [X, Z] = rotar_perfil(X, Z, alpha)
+
+    % Convertim l'angle a radians
+    alpha_rad = deg2rad(alpha);
+    
+    % Matriu de rotació
+    R = [cos(alpha_rad), sin(alpha_rad);
+         -sin(alpha_rad), cos(alpha_rad)];
+     
+    % Coordenades rotades
+    coords = R * [X; Z];
+    
+    % Retornar les noves coordenades
+    X = coords(1, :);
+    Z = coords(2, :);
+
+    % Visualitzar el perfil
+    figure;
+    plot(X, Z, '-o');
+    title(['Perfil NACA 0012 amb angle d''atac \alpha = ', num2str(alpha), '°']);
+    xlabel('X');
+    ylabel('Z');
+    grid on;
+    axis equal;
+end
