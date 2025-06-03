e0eba93b4f840ab4a53f0f24c174daf86e1bce0b
Pau Cornudella Quer <161316282+PauCoqu@users.noreply.github.com>
2025-06-04 00:00:19 +0200

    ra

--- calcul_gama.m
+++ calcul_gama.m
@@ -1,32 +1,34 @@
-function [gama_centre_panell] = calcul_gama(alpha_ala, N,Coords_ala)
+function [gama_centre_panell] = calcul_gama(c_ala, alpha_ala, Cl_0_ala, Cl_alpha_ala, N, i_ala)
  
+%c_ala(i) = corda ala o canard (c_ala, c_canard....)
+%alpha_ala = angle ala o canard (alpha_ala, .....)
+%Cl_0_ala = Cl_0_0012, Cl_0_22112 ....
+%Cl_alpha_ala = Cl_alpha_0012, Cl_alpha_22112 ....
+%i_ala = incidencia ala o canard (i_w, i_h ....)
+%twist(i) = twist_centre_panell....
+
 a_ij = zeros(N, N); %pag 19
 b_i = zeros(N, 1); %pag 19
 
 %Referencia_k = Vector perpendicular a Q_inf. Referencia_k = (-a,0,b) 
-Referencia_k = [-sin(alpha_ala + inc); 0; cos(alpha_ala + inc)]; %(pag 16)
+Referencia_k = [-sin(alpha_ala + i_ala); 0; cos(alpha_ala + i_ala)]; %(pag 16)
 
 for i = 1:N
 
-b_i(i) = 0.5 * c(i) * Q_inf * (Cl0 + Cl_alpha_ala * (alpha_ala + t(i) + inc));
+b_i(i) = (1/2)*c_ala(i)*Q_inf*(Cl_0_ala + Cl_alpha_ala*(alpha_ala + i_ala + twist(i))); %(pag 19)
 
     for j = 1:N
         if j == i
-             % Velocidad inducida por el propio vórtice (self-induced)
-                V_ii = self_vortex(Xc(:, i), X(:, j), X(:, j + 1), Ur);
-                a_ww(i, j) = -0.5 * Cl_alpha_ala * c(i) * dot(V_ii, k) + 1;
+                V_ii = self_vortex(Xc(:, i), X(:, j), X(:, j + 1), Ur); %Velocitat autoinduida
+                a_ij(i, j) = (-1/2)*Cl_alpha_ala*c_ala(i)*dot(V_ii, k)+1; %(pag 19) Vel
         else
-                % Velocidad inducida por otros vórtices (interacción)
-                V_ij = horseshoe_vortex(Xc(:, i), X(:, j), X(:, j + 1), Ur);
-                a_ww(i, j) = -0.5 * Cl_alpha_ala * c(i) * dot(V_ij, k);
+                V_ij = horseshoe_vortex(Xc(:, i), X(:, j), X(:, j + 1), Ur); %Velocitat induida
+                a_ij(i, j) = (-1/2)*Cl_alpha_ala * c(i) * dot(V_ij, k); %(pag 19) 
         end
     end
 end
 
-% Resolución del sistema lineal A * gamma = b
-gamma_isol = a_ww \ b_ww;
-
-
-%gama_centre_panell = 1;
+% A*gamma = b
+gama_centre_panell = a_ij \ b_ij;
 
 end
